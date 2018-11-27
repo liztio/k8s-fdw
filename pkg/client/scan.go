8a32641cef8d661a63272d6210eff12e8a3df69d
liz <liz@heptio.com>
2018-11-27 17:09:46 -0500

    Table scanner

---
+++ pkg/client/scan.go
@@ -0,0 +1,49 @@
+package client
+
+import (
+	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/dynamic"
+)
+
+// ScanState represents the state of one particular scan
+type ScanState struct {
+	client    dynamic.NamespaceableResourceInterface
+	namespace string
+	list      *unstructured.UnstructuredList
+	curRow    int
+	fields    []string
+}
+
+// Next returns the next value, or an empty list if there's no more left
+func (s *ScanState) Next() ([]interface{}, error) {
+	if s.list == nil {
+		if err := s.retrieve(); err != nil {
+			return []interface{}{}, err
+		}
+	}
+
+	// TODO(EKF): handle continue
+	if len(s.list.Items) <= s.curRow {
+		return []interface{}{}, nil
+	}
+
+	row, err := AsRows(&s.list.Items[s.curRow], s.fields)
+	if err != nil {
+		return []interface{}{}, errors.Wrap(err, "couldn't construct row")
+	}
+
+	s.curRow++
+	return row, nil
+}
+
+func (s *ScanState) retrieve() error {
+	var err error
+	if s.namespace == "" {
+		s.list, err = s.client.List(metav1.ListOptions{})
+	} else {
+		s.list, err = s.client.Namespace(s.namespace).List(metav1.ListOptions{})
+	}
+	return errors.Wrap(err, "couldn't retrieve objects")
+}
---
+++ pkg/client/scan_test.go
@@ -0,0 +1,79 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic/fake"
+)
+
+func TestNext(t *testing.T) {
+	pod := yamlToUnstructured(t, testPod)
+	client := fake.NewSimpleDynamicClient(runtime.NewScheme(), pod)
+
+	tests := []struct {
+		name     string
+		state    *ScanState
+		expected []interface{}
+	}{
+		{
+			name: "empty list",
+			state: &ScanState{
+				list: &unstructured.UnstructuredList{},
+			},
+		},
+		{
+			name: "end of list",
+			state: &ScanState{
+				list: &unstructured.UnstructuredList{
+					Items: []unstructured.Unstructured{
+						unstructured.Unstructured{},
+					},
+				},
+				curRow: 1,
+			},
+		},
+		{
+			name: "one item",
+			state: &ScanState{
+				list: &unstructured.UnstructuredList{
+					Items: []unstructured.Unstructured{
+						*pod,
+					},
+				},
+				fields: []string{"metadata.name", "metadata.namespace"},
+			},
+			expected: []interface{}{"myapp-pod", "test-namespace"},
+		},
+		{
+			name: "retrieve options",
+			state: &ScanState{
+				client: client.Resource(schema.GroupVersionResource{
+					Group:    "",
+					Version:  "v1",
+					Resource: "pods",
+				}),
+				fields:    []string{"metadata.name", "metadata.namespace"},
+				namespace: "test-namespace",
+			},
+			expected: []interface{}{"myapp-pod", "test-namespace"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			a := assert.New(t)
+			vals, err := test.state.Next()
+			if a.NoError(err) {
+				if test.expected == nil {
+					a.Empty(vals)
+				} else {
+					a.Equal(test.expected, vals)
+				}
+			}
+		})
+	}
+}
