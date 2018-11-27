e8593376ab8fb0f6fe8a50acfc1df1b3e79e58f9
liz <liz@heptio.com>
2018-11-27 14:27:38 -0500

    Test of converting k8s objects into rows

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,17 +1,21 @@
 package client
 
 import (
-	// include auth methods
+	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/third_party/forked/golang/template"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/jsonpath"
 )
 
 // Client represents
@@ -66,10 +70,64 @@ func newAPIHelperFromRESTConfig(cfg *rest.Config) (meta.RESTMapper, error) {
 	return restmapper.NewDiscoveryRESTMapper(groupResources), nil
 }
 
+// GetResourceForKind turns a GroupVersionKind into a GroupVersionResource
 func (c *Client) GetResourceForKind(gvk *schema.GroupVersionKind) (*schema.GroupVersionResource, error) {
 	mapping, err := c.mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
 	if err != nil {
 		return nil, errors.Wrapf(err, "couldn't get resource for %+v", gvk)
 	}
 	return &mapping.Resource, nil
+}
+
+// AsRows takes an object and turns it into a single k8s row.
+func AsRows(obj *unstructured.Unstructured, columns []string) ([]interface{}, error) {
+	row := make([]interface{}, len(columns))
+
+	for i, col := range columns {
+		// Is it JSONPath?
+		if strings.HasPrefix(col, "{") && strings.HasSuffix(col, "}") {
+			jp := jsonpath.New(col)
+			// TODO(EKF): Cache these parse results
+			if err := jp.Parse(col); err != nil {
+				return []interface{}{}, errors.Wrapf(err, "couldn't parse template %q", col)
+			}
+			results, err := jp.FindResults(obj.Object)
+			if err != nil {
+				return []interface{}{}, errors.Wrapf(err, "failed to execute template %q", col)
+			}
+
+			if len(results) == 0 {
+				continue
+			}
+
+			// extract results from the rat's nest of reflect.Value
+			vals := make([]interface{}, len(results[0]))
+			for i, resVal := range results[0] {
+				val, ok := template.PrintableValue(resVal)
+				if !ok {
+					return []interface{}{}, fmt.Errorf("couldn't print result %v", results)
+				}
+				vals[i] = val
+			}
+
+			if len(vals) == 1 {
+				row[i] = vals[0]
+			} else {
+				row[i] = vals
+			}
+
+			continue
+		}
+
+		val, found, err := unstructured.NestedFieldCopy(obj.Object, strings.Split(col, ".")...)
+		if err != nil {
+			return []interface{}{}, errors.Wrap(err, "couldn't traverse object")
+		}
+		if !found {
+			return []interface{}{}, fmt.Errorf("no such field %q", col)
+		}
+		row[i] = val
+	}
+
+	return row, nil
 }
--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -6,10 +6,45 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/kubernetes/scheme"
 )
 
 const validKubeconfig = "testdata/kubeconfig"
+
+var (
+	testJob = `apiVersion: batch/v1
+kind: Job
+metadata:
+  name: pi
+spec:
+  template:
+    spec:
+      containers:
+      - name: pi
+        image: perl
+        command: ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+      restartPolicy: Never
+  backoffLimit: 4
+`
+	testPod = `apiVersion: v1
+kind: Pod
+metadata:
+  namespace: test-namespace
+  name: myapp-pod
+  labels:
+    app: myapp
+spec:
+  containers:
+  - name: myapp-container
+    image: busybox
+    command: ['sh', '-c', 'echo Hello Kubernetes! && sleep 3600']
+  - name: myapp-container2
+    image: postgresql
+`
+)
 
 func TestGetClientNonexistentFile(t *testing.T) {
 	file := "some nonexistent file"
@@ -65,6 +100,7 @@ func TestGetResourceForKind(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			a := assert.New(t)
 			resource, err := client.GetResourceForKind(test.kind)
 
 			if a.NoError(err) {
@@ -72,6 +108,86 @@ func TestGetResourceForKind(t *testing.T) {
 				a.Equal(resource.Version, test.resource.Version, "version did not match")
 				a.Equal(resource.Resource, test.resource.Resource, "resource did not match")
 
+			}
+		})
+	}
+}
+
+func yamlToUnstructured(t *testing.T, yaml string) *unstructured.Unstructured {
+	obj := &unstructured.Unstructured{}
+	if err := runtime.DecodeInto(scheme.Codecs.UniversalDecoder(), []byte(yaml), obj); err != nil {
+		t.Fatalf("couldn't decode template: %v", err)
+	}
+	return obj
+}
+
+func TestAsRows(t *testing.T) {
+	pod := yamlToUnstructured(t, testPod)
+	job := yamlToUnstructured(t, testJob)
+
+	tests := []struct {
+		name      string
+		obj       *unstructured.Unstructured
+		rows      []string
+		expected  []interface{}
+		expectErr bool
+	}{
+		{
+			name:     "basic pod",
+			obj:      pod,
+			rows:     []string{"metadata.name", "metadata.namespace"},
+			expected: []interface{}{"myapp-pod", "test-namespace"},
+		},
+		{
+			name:     "basic job",
+			obj:      job,
+			rows:     []string{"metadata.name", "spec.backoffLimit"},
+			expected: []interface{}{"pi", int64(4)},
+		},
+		{
+			name:      "unknown field",
+			obj:       job,
+			rows:      []string{"nonsense"},
+			expectErr: true,
+		},
+		{
+			name:     "jsonpath",
+			obj:      pod,
+			rows:     []string{"{.spec.containers[0].image}"},
+			expected: []interface{}{"busybox"},
+		},
+		{
+			name:      "malformed jsonpath",
+			obj:       pod,
+			rows:      []string{"{.spec.container[}"},
+			expectErr: true,
+		},
+		{
+			name:      "jsonpath for unknown field",
+			obj:       job,
+			rows:      []string{"{.spec.somethingelse}"},
+			expectErr: true,
+		},
+		{
+			name:     "jsonpath returning list",
+			obj:      pod,
+			rows:     []string{"{.spec.containers[*].image}"},
+			expected: []interface{}{[]interface{}{"busybox", "postgresql"}},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			a := assert.New(t)
+
+			result, err := AsRows(test.obj, test.rows)
+			if test.expectErr {
+				a.Error(err)
+				return
+			}
+
+			if a.NoError(err) {
+				a.Equal(test.expected, result)
 			}
 		})
 	}
