5ca602c46f39f167c9cdd9e277c21132458752fc
liz <liz@heptio.com>
2018-11-27 13:01:45 -0500

    Update the tests

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -5,8 +5,12 @@ import (
 	"io/ioutil"
 
 	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -14,10 +18,12 @@ import (
 type Client struct {
 	restcfg *rest.Config
 	dyn     dynamic.Interface
+	mapper  meta.RESTMapper
 }
 
 // GetClient returns a client for the given kubectl path
-func GetClient(path string) (*Client, error) {
+// TODO: getMapper is a dirty hack to make tests work
+func GetClient(path string, getMapper bool) (*Client, error) {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, errors.Wrap(err, "couldn't read file")
@@ -32,8 +38,38 @@ func GetClient(path string) (*Client, error) {
 		return nil, errors.Wrapf(err, "couldn't create dynamic client from %s", path)
 	}
 
-	return &Client{
+	client := &Client{
 		restcfg: restcfg,
 		dyn:     dyn,
-	}, nil
+	}
+
+	if getMapper {
+		mapper, err := newAPIHelperFromRESTConfig(restcfg)
+		if err != nil {
+			return nil, errors.Wrapf(err, "couldn't get resource mapper")
+		}
+		client.mapper = mapper
+	}
+
+	return client, nil
+}
+
+func newAPIHelperFromRESTConfig(cfg *rest.Config) (meta.RESTMapper, error) {
+	discover, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create discovery client")
+	}
+	groupResources, err := restmapper.GetAPIGroupResources(discover)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not get api group resources")
+	}
+	return restmapper.NewDiscoveryRESTMapper(groupResources), nil
+}
+
+func (c *Client) GetResourceForKind(gvk *schema.GroupVersionKind) (*schema.GroupVersionResource, error) {
+	mapping, err := c.mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return nil, errors.Wrapf(err, "couldn't get resource for %+v", gvk)
+	}
+	return &mapping.Resource, nil
 }
--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -5,13 +5,15 @@ import (
 	"testing"
 
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 const validKubeconfig = "testdata/kubeconfig"
 
 func TestGetClientNonexistentFile(t *testing.T) {
 	file := "some nonexistent file"
-	_, err := GetClient(file)
+	_, err := GetClient(file, false)
 	if err == nil {
 		t.Fatalf("expected err, got nil")
 	}
@@ -22,12 +24,55 @@ func TestGetClientNonexistentFile(t *testing.T) {
 }
 
 func TestGetClientExistentFile(t *testing.T) {
-	kubecfg, err := GetClient(validKubeconfig)
+	kubecfg, err := GetClient(validKubeconfig, false)
 	if err != nil {
 		t.Fatalf("unexpected err: %v", err)
 	}
 
 	if kubecfg.restcfg.Host != "https://localhost:6443" {
 		t.Errorf("expected host to be localhost, was %q", kubecfg.restcfg.Host)
+	}
+}
+
+func TestGetResourceForKind(t *testing.T) {
+	t.Skip("needs a valid kubeconfig")
+	a := assert.New(t)
+
+	tests := []struct {
+		name     string
+		kind     *schema.GroupVersionKind
+		resource *schema.GroupVersionResource
+	}{
+		{
+			name: "deployment",
+			kind: &schema.GroupVersionKind{
+				Group:   "apps",
+				Version: "v1beta1",
+				Kind:    "Deployment",
+			},
+			resource: &schema.GroupVersionResource{
+				Group:    "extensions",
+				Version:  "v1beta1",
+				Resource: "deployments",
+			},
+		},
+	}
+
+	client, err := GetClient(validKubeconfig, true)
+	if !a.NoError(err) {
+		t.FailNow()
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			resource, err := client.GetResourceForKind(test.kind)
+
+			if a.NoError(err) {
+				a.Equal(resource.Group, test.resource.Group, "group did not match")
+				a.Equal(resource.Version, test.resource.Version, "version did not match")
+				a.Equal(resource.Resource, test.resource.Resource, "resource did not match")
+
+			}
+		})
 	}
 }
